5f3bf7fd0fc65e41abb1feb4fd755d721bea67be
Your Nameariel <arielzissu98@gmail.com>
2020-04-23 11:55:00 +0300

    Updates

---
+++ precache-manifest.3fb16a9c662f5f349ae1a4d759d4fc85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d568f7953372556564f2cb4827b86b92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e94a66210eb67dde444e",
+    "url": "/static/css/main.24aa8f67.chunk.css"
+  },
+  {
+    "revision": "4aeda01ec5bb0f58e421",
+    "url": "/static/js/2.a1bafac6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a1bafac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e94a66210eb67dde444e",
+    "url": "/static/js/main.7a6bb9d1.chunk.js"
+  },
+  {
+    "revision": "ed1f7dc65b8aad28def3",
+    "url": "/static/js/runtime-main.b769f686.js"
+  },
+  {
+    "revision": "5b025a51d20fec85948f10d955078897",
+    "url": "/static/media/RobotoSlab-Bold.5b025a51.ttf"
+  }
+]);
